770ea40fa6fee493c404a8de5f47830e3323df59
HayashiKana <hpns.3537fs.kana@gmail.com>
2023-12-04 23:27:40 +0900

    bug fix

--- kana.py
+++ kana.py
@@ -2,10 +2,7 @@ print("hello world")
 
 print("good night")
 
-<<<<<<< HEAD
 print("goodbye")
-=======
 for i in range(10):
     print("Hello world")
->>>>>>> refs/remotes/origin/main
 print("hello")
