f6cd167d64666d4cbb0fb3bafa02b9ad99385ccb
kana265 <136039889+kana265@users.noreply.github.com>
2024-04-10 22:18:08 +0900

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void main(String[] args)
+    {
+        String msg = "";
+        msg += "hello";
+        msg += "world!";
+        System.out.println(msg);
+    }
+}
