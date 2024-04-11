7e9e87f43399d9e2a0617e89c2e21612968ffaf1
kana265 <136039889+kana265@users.noreply.github.com>
2024-04-11 18:49:33 +0900

    Add book score

--- Main.java
+++ Main.java
@@ -1,9 +1,35 @@
+import java.util.Scanner;
+
 class Main{
+
     public static void main(String[] args)
     {
-        String msg = "";
-        msg += "hello";
-        msg += "world!";
-        System.out.println(msg);
+        Scanner scanner = new Scanner(System.in);
+
+        int countUsers = scanner.nextInt();
+        int countBooks = scanner.nextInt();
+
+        double [][] scores = new double[countUsers][countBooks];
+
+        for(int i=0; i<countUsers; i++){
+            for(int j=0; j<countBooks; j++){
+                scores[i][j] = scanner.nextDouble();
+            }
+        }
+
+        
+
+        for (int i = 0; i < countUsers; i++) {
+            System.out.print("Student " + (i + 1) + ": ");
+            for (int j = 0; j < countBooks; j++) {
+                if (scores[i][j] < 0) {
+                    System.out.print("Absent ");
+                } else {
+                    System.out.print(scores[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+        scanner.close();
     }
 }
