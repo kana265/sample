49e740284eb943b185a1f03fe96f2cb4998ec8df
kana265 <136039889+kana265@users.noreply.github.com>
2024-04-12 17:00:40 +0900

    add searching related User section.

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 class Main{
 
@@ -9,27 +10,45 @@ class Main{
         int countUsers = scanner.nextInt();
         int countBooks = scanner.nextInt();
 
-        double [][] scores = new double[countUsers][countBooks];
+        double [][] review = new double[countUsers][countBooks];
+        double [] dist = new double [countUsers-1];
+        double distance = 0;
+        double [] score = new double [countUsers-1];
+        
 
         for(int i=0; i<countUsers; i++){
             for(int j=0; j<countBooks; j++){
-                scores[i][j] = scanner.nextDouble();
+                review[i][j] = scanner.nextDouble();
             }
         }
 
-        
-
-        for (int i = 0; i < countUsers; i++) {
-            System.out.print("Student " + (i + 1) + ": ");
-            for (int j = 0; j < countBooks; j++) {
-                if (scores[i][j] < 0) {
-                    System.out.print("Absent ");
-                } else {
-                    System.out.print(scores[i][j] + " ");
-                }
+        for (int i=1; i<countUsers; i++){
+           for(int j=0; j<countBooks; j++){
+            if(review[0][j] < 0 || review[i][j] < 0)
+            {
+                continue;
+            }else
+            {
+                distance = distance + (review[0][j] - review[i][j]) * (review[0][j] - review[i][j]);
+                System.out.println("a^2 + b^2) = " + distance + " ");
             }
-            System.out.println();
+           }
+           
+           dist[i-1] = Math.sqrt(distance);
+           System.out.println(" heihocon " + dist[i-1] + " ");
+           distance = 0;
+
+           score[i-1] = 1/(dist[i-1] + 1);
+           System.out.println(" score is " + score[i-1] + " ");
+           
+
         }
+
+        Arrays.sort(score);
+        for(int i=0; i<countUsers-1; i++){
+            System.out.println( i + " " + score[i] + " ");
+        }
+
         scanner.close();
     }
 }
