7cec9f93ecad8d5ca87a6066c3f2181402c2bfff
kana265 <136039889+kana265@users.noreply.github.com>
2024-04-18 19:51:44 +0900

    add book class

---
+++ Book.java
@@ -0,0 +1,13 @@
+
+public class Book {
+    private int bookId;
+
+    public Book(int bookId){
+        this.bookId = bookId;
+    }
+
+    public int getBookId(){
+        return bookId;
+    }
+
+}
