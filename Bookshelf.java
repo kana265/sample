3a21eda3a096b0dfcdf12388fa49d24e8b5760b1
kana <136039889+kana265@users.noreply.github.com>
2024-04-27 20:33:53 +0900

    Add files via upload

--- Bookshelf.java
+++ Bookshelf.java
@@ -1,13 +1,27 @@
-public class Bookshelf {
-    private int[] books;
+import java.util.ArrayList;
 
-    public Bookshelf(int[] books ){
-        this.books = books;
+class Bookshelf implements IAggregate {
+
+    private ArrayList<Book> bookList;
+
+    public Bookshelf(int num){
+        this.bookList = new ArrayList<>(num);
     }
 
-    public int[] getBooks( ){
-        return books;
+    public int getLength() {
+        return bookList.size();
     }
 
+    public void appendBook(Book book) {
+        bookList.add(book);
+    }
+
+    public Book getBookAt(int index) {
+        return bookList.get(index);
+    }
+
+    @Override
+    public IIterator createIterator() {
+        return new BookshelfIterator(this);
+    }
 }
-
